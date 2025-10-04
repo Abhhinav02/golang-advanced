fc5ff18c5f7c074dc2c43dd64d7e3db12d701e4b
Soumadip <39thdecember@gmail.com>
2025-10-04 23:27:21 +0530

    multiplexing using select 🚀

--- main.go
+++ main.go
@@ -1,30 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+ func main() {
+	ch := make(chan int)
+
+	go func(){
+		ch <-1
+		close(ch)
+	}()
+
+	for {
+		select{
+		case msg,ok:= <-ch:
+			if !ok{
+				fmt.Println("Channel closed 🔴")
+				// cleanup activities.. 🧹
+				return
+			}
+			fmt.Println("Received:",msg)
+		}
+	}
+ }	
+
+
+
+//  func main() {
+// 	ch := make(chan int)
+
+// 	go func(){
+// 		time.Sleep(2* time.Second) 
+// 		ch <-1
+// 		close(ch)
+// 	}()
+
+// 	select {
+// 	case msg := <-ch:
+// 		fmt.Println("✅Received from ch:",msg)
+// 	case <- time.After(3* time.Second):
+// 		fmt.Println("⏳ Timeout.. ")
+// 	}
+//  }
+
 
 // multiplexing - kinda like switch-case
-func main() {
-	ch1:= make(chan int)
-	ch2:= make(chan int)
+// func main() {
+// 	ch1:= make(chan int)
+// 	ch2:= make(chan int)
 
-	//! Err - DEADLOCK ⚠️
-	// msg1:= <-ch1
-	// fmt.Println("✅ Received from ch1:",msg1)
+// 	go func(){
+// 		time.Sleep(time.Second)
+// 		ch1 <- 1
+// 	}()
 
-	// msg2:= <-ch2
-	// fmt.Println("☑️ Received from ch2:",msg2)
+// 	go func(){
+// 		time.Sleep(time.Second)
+// 		ch2 <- 2
+// 	}()
 
-	//! SOLUTION 🚀
-	select {
-	case msg:= <-ch1:
-	fmt.Println("✅ Received from ch1:",msg)
 
-	case msg:= <-ch2:
-	fmt.Println("☑️ Received from ch2:",msg)
+// 	time.Sleep(2* time.Second) // Waiting..
 
-	default:
-	fmt.Println("🔴 Default Case - No channels ready! ")
-	}
+// 	for range 2{
+// 	select {
+// 	case msg:= <-ch1:
+// 	fmt.Println("✅Received from ch1:",msg)
 
-	//fmt.Println("End of the program.. 🔴")
-}
+// 	case msg:= <-ch2:
+// 	fmt.Println("☑️Received from ch2:",msg)
+
+// 	default:
+// 	fmt.Println("🔴 Default Case - No channels ready! ")
+// 	}
+// }
+
+
+// 	fmt.Println("###### End of the program ######")
+// }
