2ac5f5a55ee424b528eeeb38199b88b6a268626b
Soumadip <39thdecember@gmail.com>
2025-10-08 16:31:16 +0530

    periodic-task execution

--- main.go
+++ main.go
@@ -5,22 +5,33 @@ import (
 	"time"
 )
 
-func main() {
-	ticker := time.NewTicker(time.Second)
+func periodicTask(){
+	fmt.Println("⌛ Performing periodic task at:",time.Now())
+}
 
-	i:=0;
-	for range ticker.C{
-		i++
-		fmt.Println(i)
+func main() {
+	// Periodic Task exec()
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop() // Turn off ticker
+
+	for{
+	select{
+	case <-ticker.C:
+		periodicTask()
+	}
 	}
 
-	// O.p
-	// $ go run .
-	// 1
-	// 2
-	// 3
-	// 4
-	// 5
-	// ... So on...
+	
+
+// O.p
+// $ go run .
+// 	⌛ Performing periodic task at: 2025-10-08 16:28:24.8883307 +0530 IST m=+6.001331601
+// ⌛ Performing periodic task at: 2025-10-08 16:28:25.8885089 +0530 IST m=+7.001509801
+// ⌛ Performing periodic task at: 2025-10-08 16:28:26.8879589 +0530 IST m=+8.000959801
+// ⌛ Performing periodic task at: 2025-10-08 16:28:27.8880838 +0530 IST m=+9.001084701
+// ⌛ Performing periodic task at: 2025-10-08 16:28:28.8878862 +0530 IST m=+10.000887101
+// ⌛ Performing periodic task at: 2025-10-08 16:28:29.8882013 +0530 IST m=+11.001202201
+// .... so on...
+	
 
 }
