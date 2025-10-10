bac337239e6320cd12e4377b2c77bed83fba73e4
Soumadip <39thdecember@gmail.com>
2025-10-10 20:55:58 +0530

    protect resources using mutex ☑️

--- main.go
+++ main.go
@@ -5,46 +5,37 @@ import (
 	"sync"
 )
 
-/*
-💡mutexes - mitual exclution, sync. primitive which prevents multiple goroutines from simultaneously accessing shared resources or exeuting critical sections of the code. Ensures that only 1 goroutine can hold the 'mutex' at a time, thus avaoiding race-conditions and data-corruption. 💻
-*/
-
-type Counter struct{
-	mu sync.Mutex
-	val int
-}
-
-func (c *Counter)increment(){
-	c.mu.Lock()
-	c.val++
-	defer c.mu.Unlock()
-}
-
-func (c *Counter) getVal()int{
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	return c.val
-}
+//💡mutexes - how do they understand which values to protect?
 
 func main() {
-	// waitgroups for multiple goroutines
+
+	var counter int
 	var wg sync.WaitGroup
-	counter:= &Counter{}
-	numOfGoroutines := 10
+
+	var mu sync.Mutex
+
+	numOfGoroutines:=5
+	wg.Add(numOfGoroutines)
+
+	increment:= func ()  {
+		// can be used inside loops too
+		defer wg.Done()
+		for range 1000{
+			mu.Lock()
+			counter++
+			mu.Unlock()
+		}
+	}
 
 	for range numOfGoroutines{
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for range 100{
-				counter.increment()
-			}
-		}()
+		go increment()
 	}
-	wg.Wait()
-	fmt.Printf("✅ Final counter val: %d\n",counter.getVal()) // ⚠️unreliable output without 'mutex'
 
+	wg.Wait()
+	fmt.Printf("✅ Final counter val: %d\n",counter)
+	
 	// O.P- 
 	// $ go run .
-	// ✅ Final counter val: 1000
+	// ✅ Final counter val: 5000
+	
 }
